a2b5f0089e1a530aec514196d22e77af7a2aed9d
Quica <blogencolores@gmail.com>
2021-01-25 23:10:48 +0100

    Delete backend directory

--- backend/src/models/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-
-    Book: require("./Book"),
-    Contact : require("./Contact"),
-    Drawing: require("./Drawing"),
-    Line: require("./Line"),
-    Point: require("./Point"),
-    User: require("./User")
-};
